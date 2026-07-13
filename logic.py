16c5ed481109fe078df53d16fe9abc1c98a6a47d
Jonas <88602274+aAsiong@users.noreply.github.com>
2026-07-13 18:04:51 +0800

    Added gui.py callback

--- logic.py
+++ logic.py
@@ -1,13 +1,17 @@
 from html.parser import HTMLParser
+import pandas as pd
 
 class MyHTMLParser(HTMLParser):
     def __init__(self, log_callback):
         super().__init__()
         self.in_table = False
-        self.row_count = 0
         self.in_row = False
         self.in_cell = False
-        self.current_row_data = []
+        self.row_count = 0
+        self.col_index = 0
+        self.columns = ["web_id", "date_requested", "requestor", "form", "description"]
+        self.current_col = ""
+        self.current_row_data = {}
         self.all_extracted_data = []
         self.log = log_callback
 
@@ -16,8 +20,6 @@ class MyHTMLParser(HTMLParser):
     
     def handle_starttag(self, tag, attrs):
         attr_dict = dict(attrs)
-
-        self.current_tag = tag
         get_tag = tag.upper()
         
         if (get_tag == "TABLE"):
@@ -27,35 +29,39 @@ class MyHTMLParser(HTMLParser):
             self.in_row = True
             self.row_count += 1
             
-            print(f"Row {self.row_count}: processing begin")
-            self.log(f"Row {self.row_count}: processing begin")
-
-            self.current_row_data = []
+            self.log("1", f"Row {self.row_count}: processing begin")
+            self.current_row_data = {}
+            self.col_index = 0
             
         elif (get_tag == "TD" and self.in_row == True):
             if (self.row_count <= 1):
+                self.log("0", "====Skipping Header====")
                 return
             
             self.in_cell = True
+            if (self.col_index == 0):
+                return
 
-        elif  (get_tag == "A" and self.in_cell == True):
+            self.current_col = self.columns[self.col_index - 1]
+                
+        elif  (get_tag == "A" and self.in_cell == True and self.col_index == 1):
             if ("href" in attr_dict):
                 if (attr_dict["href"] is None):
-                    print(f"Row {self.row_count}: No URL found in web column")
+                    self.log("2", f"Row {self.row_count}: No URL found in web column")
                 else:
                     extracted_id = self.extract_notes_id_from_url(attr_dict["href"])
-                    self.current_row_data.append(extracted_id)
+                    self.current_row_data[self.current_col ] = extracted_id
             else:
-                print(f"Row {self.row_count}: No HREF attribute")
-
+                self.log("2", f"Row {self.row_count}: No HREF attribute")
         else:
             return
 
     def handle_data(self, data):
-        if (self.in_cell == True and self.row_count > 1):
+        if (self.in_cell == True and self.row_count > 1 and self.col_index > 0):
             clean_data = data.strip()
             if (clean_data and clean_data != "Notes" and clean_data != "Web"):
-                self.current_row_data.append(clean_data)
+                self.current_row_data[self.current_col] = clean_data
+        self.col_index += 1
 
     def handle_endtag(self, tag):
         get_tag = tag.upper()
@@ -64,7 +70,7 @@ class MyHTMLParser(HTMLParser):
             self.in_table = False
 
         elif (get_tag == "TR" and self.in_table):
-            if self.in_row and self.row_count > 1:
+            if (self.in_row and self.row_count > 1):
                 self.all_extracted_data.append(self.current_row_data)
             self.in_row = False
 
@@ -74,7 +80,4 @@ class MyHTMLParser(HTMLParser):
 def process_input_call(data, log_callback):
     parser = MyHTMLParser(log_callback)
     parser.feed(data)
-    return parser.all_extracted_data
-    
-def clear_input_call():
-    print("hello Franzene")
+    pandas_dt_frame(parser.all_extracted_data)
